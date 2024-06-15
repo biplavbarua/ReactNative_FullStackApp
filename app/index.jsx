ecab30a8d47fcd9c9b6cc7a3650ba8cbba410be7
biplavbarua <innocentsavage.bb@gmail.com>
2024-06-15 17:56:40 +0530

    Auth-Screens Completed

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Text, View, Image, ScrollView } from 'react-native';
-import { Link } from 'expo-router';
+import { Redirect, router } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { images } from '../constants';
+import CustomButton from '../components/CustomButton';
 
 export default function App() {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: '100%'}}>
-        <View className="w-full justify-center align-center items-center h-full px-4">
+        <View className="w-full justify-center align-center items-center min-h-[85vh] px-4">
           <Image 
             source={images.logo}
             className="w-[130px] h-[84px]"
@@ -39,8 +40,15 @@ export default function App() {
             Where creativity meets innovation: Embark on a journey of limitless exploration with Aora.
           </Text>
 
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => router.push('/sign-in')}
+            containerStyles="w-full mt-7"
+          />
+
         </View>
       </ScrollView>
+      <StatusBar backgroundColor="#161622" style="light" />
     </SafeAreaView>
   );
 }
